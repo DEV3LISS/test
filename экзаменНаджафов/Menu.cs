c1a19235c66cc5725361287fe50dded4e1bf5621
student304 <student304@304-02>
2021-05-19 11:42:42 +0500

    3

---
+++ экзаменНаджафов/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace экзаменНаджафов
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void clientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ckients ckients = new ckients();
+            ckients.Show();
+
+
+        }
+    }
+}
--- экзаменНаджафов/ckients.cs
+++ экзаменНаджафов/ckients.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace экзаменНаджафов
 {
-    public partial class Form1 : Form
+    public partial class ckients : Form
     {
-        public Form1()
+        public ckients()
         {
             InitializeComponent();
         }
